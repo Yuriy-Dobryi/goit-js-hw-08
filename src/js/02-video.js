377ee05db631cab9f8ec4baa8ff9adc1bc9bf127
Yuriy-Dobryi <yuriy.dobryi@gmail.com>
2023-03-26 01:40:50 +0200

    almost done task_2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,19 @@
 import Player from '@vimeo/player';
+import _throttle from 'lodash.throttle';
 
-  const iframe = document.querySelector('iframe');
-  const player = new Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const videoCurrentTime = localStorage.getItem('videoplayer-current-time');
 
-// player.on('timeupdate', function () {
-//   console.log('played the video!');
-// });
+if (videoCurrentTime) {
+  player.setCurrentTime(videoCurrentTime);
+}
 
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
+player.on('timeupdate', _throttle(({ seconds }) =>
+  localStorage.setItem('videoplayer-current-time', seconds), 1000));
+
+// try {
+//   player.setCurrentTime(videoCurrentTime);
+// } catch (error) {
+//   console.log(error);
+// }
